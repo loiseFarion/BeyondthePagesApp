14dd8c2ef43eb27fa2ff16c25dac5dd1bb457656
Loise Farion <loise.farion@volvo.com>
2025-02-14 10:56:18 -0300

    Add employee overview component and mock data service

---
+++ BeyondthePagesApp/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,14 @@
+﻿using BeyondthePagesApp.Library.Domain;
+using BeyondthePagesApp.Service;
+
+namespace BeyondthePagesApp.Components.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee> Employees { get; set; } = default!;
+        protected async override Task OnInitializedAsync()
+        {
+            Employees = MockDataService.Employees;
+        }
+    }
+}
