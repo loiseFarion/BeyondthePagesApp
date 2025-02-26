d851e5c06f4ee55de9ab35e2326849c365c7c9e4
Loise Farion <loise.farion@volvo.com>
2025-02-26 10:16:45 -0300

    Add layout and pages of employee

---
+++ BeyondthePagesApp/Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,21 @@
+﻿using BeyondthePagesApp.Library.Domain;
+using BeyondthePagesApp.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace BeyondthePagesApp.Components.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+        protected override void OnInitialized()
+        {
+            Employee = MockDataService.Employees.Single(e => e.EmployeeId == EmployeeId);
+        }
+        private void ChangeHolidayState()
+        {
+            Employee.IsOnHoliday = !Employee.IsOnHoliday;
+        }
+    }
+}
